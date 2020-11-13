a3a54249b949e2de35458cdc6fee1d63db6b43cf
cbackus21 <54688858+cbackus21@users.noreply.github.com>
2020-11-13 10:53:15 -0500

    Update SimpleCipher.java

--- src/SimpleCipher.java
+++ src/SimpleCipher.java
@@ -1,16 +1,44 @@
-import java.util.AbstractMap;
+import java.util.HashMap;
 
 public class SimpleCipher {
-	private AbstractMap<Character, Character> cipher;
-	private final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+	private HashMap<Character, Character> cipher;
+	private final String Alphabet = "abcdefghijklmnopqrstuvwxyz";
 	
 	public SimpleCipher() {
-		for (Character c1: alphabet.toCharArray()) {
-			cipher.put(c1, c1);
+	    char[] alphabet = Alphabet.toCharArray();
+		for (int i = 0; i < 26; i++) {
+			cipher.put(alphabet[i], alphabet[i]);
 		}
 	}
 	
-	public void editCipher(char , char d) {
-		
+	public void setCipher(String inputWord, String testWord) throws ValueAlreadyMappedException {
+		char[] input = inputWord.toCharArray();
+		char[] test = testWord.toCharArray();
+		char[] valuesMapped = {};
+		for (int i = 0; i < inputWord.length(); i++) {
+			for (char c: valuesMapped) {
+				if (test[i] == c) {
+					throw new ValueAlreadyMappedException();
+				}
+			}
+			cipher.put(input[i], test[i]);
+			valuesMapped[i] = test[i];
+		}
+	}
+	
+	public String decode(String s) {
+		char[] chars = s.toCharArray();
+		for (char c : chars) {
+			c = cipher.get(c);
+		}
+		s = chars.toString();
+		return s;
+	}
+}
+
+class ValueAlreadyMappedException extends RuntimeException {
+	
+	public ValueAlreadyMappedException() {
+		super();
 	}
 }
