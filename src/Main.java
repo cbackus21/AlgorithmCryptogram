6ded6009589d45a6f765d3691f3ac80f8ada9bb8
cbackus21 <54688858+cbackus21@users.noreply.github.com>
2020-11-13 10:52:43 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-	
+    
 	public static void main(String[] args) {
+	    CryptogramAlgorithm ca;
 		File f1 = new File("dictionary.txt");
 		Scanner sc = new Scanner(System.in);
 		while (sc.hasNextLine()) {
-			CryptogramAlgorithm ca = new CryptogramAlgorithm(sc.nextLine());
+			ca = new CryptogramAlgorithm(sc.nextLine());
 			ca.makeTrie(f1);
+			ca.testValues();
+			ca.printResults();
 		}
 		sc.close();
 	}
