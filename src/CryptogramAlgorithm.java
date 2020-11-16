867cc131dbfd887680e91db99d221ca7473df9ee
cbackus21 <54688858+cbackus21@users.noreply.github.com>
2020-11-15 21:27:09 -0500

    Update CryptogramAlgorithm.java

--- src/CryptogramAlgorithm.java
+++ src/CryptogramAlgorithm.java
@@ -1,67 +1,22 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
-public class CryptogramAlgorithm {
-	private int numDecodings = 0;
-	private Trie dictionary;
-	private ArrayList<String> inputString;
-	private ArrayList<String> currentCandidates;
-	private ArrayList<String> results;
-	private SimpleCipher cipher;
+public class CrypticWord {
+	private String word;
+	private ArrayList<String> wordCandidates;
 	
-	public CryptogramAlgorithm(String s) {
-		dictionary = new Trie();
-		inputString = new ArrayList<String>(Arrays.asList(s.split(" ")));
-		currentCandidates = new ArrayList<String>();
-		results = new ArrayList<String>();
-		
-	}
 	
-	public void makeTrie(File f1) {
-		String s;
-		try {
-			Scanner sc = new Scanner(f1);
-			while (sc.hasNextLine()) {
-				s = sc.nextLine();
-				dictionary.insert(s);
-				if (s.length() == inputString.get(0).length()) {
-					currentCandidates.add(s);
-				}
+	public CrypticWord(String s, ArrayList<String> wordCandidates) {
+	    this.wordCandidates = new ArrayList<String>();
+		this.word = s;
+		for (String word : wordCandidates) {
+			SimpleCipher sc = new SimpleCipher();
+			if (sc.setCipher(this.word, word)) {
+				this.wordCandidates.add(word);
 			}
-			sc.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		}
 	}
 	
-	public void testValues() {
-		for (String testWord : currentCandidates) {
-			try {
-				String input = inputString.get(0);
-				cipher = new SimpleCipher();
-				cipher.setCipher(input, testWord);
-				input = cipher.decode(input);
-				if (dictionary.containsNode(input)) {
-					results.add(input);
-					numDecodings++;
-				}
-			}
-			catch (ValueAlreadyMappedException e) {
-				continue;
-			}
-			catch (KeyAlreadyMappedException e) {
-				continue;
-			}
-	    }
-	}
-		
-	public void printResults() {
-		System.out.println(numDecodings);
-		for (String s: results) {
-			System.out.println(s);
-		}
+	public ArrayList<String> getWordCandidates() {
+		return this.wordCandidates;
 	}
 }
